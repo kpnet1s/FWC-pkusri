662394dfa5f8922ed0f0684764149969768c2a35
kpnet1s <mrbw.nets211@gmail.com>
2026-09-17 22:27:59 +0700

    correct file name

---
+++ Cell03/ex04/ex00bis/background.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#color-button").click(function() {
+        let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        $("body").css("background-color", color);
+    });
+});
